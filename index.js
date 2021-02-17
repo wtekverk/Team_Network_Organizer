3a1cebaea5fd219830a1b5b7e5122be93791dcf9
Wendy Tekverk <wtekverk@gmail.com>
2021-02-16 20:32:07 -0700

    added general form and files

---
+++ index.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+
+const inquirer = require('inquirer')
+
+const generateMarkdown = require('./utils/generateMarkdown')
+
+inquirer.prompt([
+
+    {
+        type: 'input',
+        message: 'What is you team name?',
+        name: 'teamName'
+    },
+
+
+//inputs go here 
+
+
+]).then((response) => {
+
+
+
+
+    const fileName = `${response.teamName}.html`;
+
+    fs.writeFile(fileName, generateMarkdown(response), (err, data) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log('Great Job!')
+            }
+        }
+
+    )
+
+
+
+}).catch(function (err) {
+    if (err) {
+        console.log("I'm so sad an error occurred. Me broken :(")
+        console.log(err);
+    }
+})
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+function generateMarkdown(response) {
+  return ` 
+  // html info goes here
+  `;
+}
+
+module.exports = generateMarkdown;
