9a5cd551694e886e292f152daac4979134ea917f
Wendy Tekverk <wtekverk@gmail.com>
2021-02-18 16:32:46 -0700

    updated enineer card template

---
+++ src/EngineerCard.js
@@ -0,0 +1,13 @@
+const EngineerCard = (profile) => `
+
+<div class="card" style="width: 18rem;">
+  <div class="card-header">
+   Manager
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">${profile.name}</li>
+    <li class="list-group-item">${profile.id}</li>
+    <li class="list-group-item">${profile.email}</li>
+    <li class="list-group-item">${profile.github}</li>
+  </ul>
+</div>`
